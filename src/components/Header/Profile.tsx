c63cfc421135f90eaa8b82a52c5d1d1aed7c69ba
Luis Ruediger <luisruediger27@gmail.com>
2022-09-23 14:40:55 -0300

    header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-        <Box mr="4" textAlign="right">
-          <Text>Luis Ruediger</Text>
-          <Text color="gray.300" fontSize="small">
-            Luis.Ruediger@gmail.com
-          </Text>
-        </Box>
+        { showProfileData && (
+          <Box mr="4" textAlign="right">
+            <Text>Luis Ruediger</Text>
+            <Text color="gray.300" fontSize="small">
+              Luis.Ruediger@gmail.com
+            </Text>
+          </Box>
+        )}
         <Avatar size="md" name="Luis Ruediger" src="https://github.com/luisruediger.png"/>
       </Flex>
   )
